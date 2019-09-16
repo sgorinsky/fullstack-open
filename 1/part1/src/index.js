62a2a7ea47bc32625089d2d9467e4ce05e3aa755
Sam Gorinsky <sgorinsky@gmail.com>
2019-09-16 18:18:20 +0200

    updated app

--- 1/part1/src/index.js
+++ 1/part1/src/index.js
@@ -150,7 +150,15 @@ const Feedback = ({good, neutral, bad}) => {
     )
 }
 
-const points = [0, 0, 0, 0, 0, 0]; 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+const points = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0);; 
 
 const App = (props) => {
     
@@ -158,18 +166,9 @@ const App = (props) => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
-    const anecdotes = [
-        'If it hurts, do it more often',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-    ];
-
+    
     const [selected, setSelected] = useState(0);
     const [max, setMax] = useState(0);
-    
     const updateMax = (current) => {
         return () => {
             ++points[current];
