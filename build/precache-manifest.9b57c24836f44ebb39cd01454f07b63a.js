9f7cda5029fbdb5d5c18d5ff453be9695140cd4a
Sam Gorinsky <sgorinsky@gmail.com>
2019-09-25 16:11:09 +0200

    prod build

---
+++ build/precache-manifest.9b57c24836f44ebb39cd01454f07b63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479adb8db38ce0ea8724a252a5fc4487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc44f792e5da9f58c93",
+    "url": "/static/css/main.d7e32b5c.chunk.css"
+  },
+  {
+    "revision": "aead7b542911353bc755",
+    "url": "/static/js/2.a82a97dd.chunk.js"
+  },
+  {
+    "revision": "ebc44f792e5da9f58c93",
+    "url": "/static/js/main.73503f07.chunk.js"
+  },
+  {
+    "revision": "0a3909685390b3afae4a",
+    "url": "/static/js/runtime~main.ce00a3d8.js"
+  }
+]);
