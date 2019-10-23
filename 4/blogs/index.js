d5f4a52f30a2a5a85c30bd02fccbd64d14c70d03
Sam Gorinsky <sgorinsky@gmail.com>
2019-10-23 09:03:32 -0400

    modified dependencies\' versions

--- 4/blogs/index.js
+++ 4/blogs/index.js
@@ -3,7 +3,6 @@ const http = require('http')
 const config = require('./utils/config')
 
 const server = http.createServer(app)
-console.log(config)
 server.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
 })
