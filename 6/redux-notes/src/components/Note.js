ef3b83cd20b2b70b93f5701c7b7a7b46640a2096
Sam Gorinsky <sgorinsky@gmail.com>
2019-11-02 21:16:13 -0500

    added button

--- 6/redux-notes/src/components/Note.js
+++ 6/redux-notes/src/components/Note.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Note = ({ note, handleClick }) => {
   return (
-    <li onClick={handleClick}>
+    <li>
       {note.content}
-      <strong>{note.important ? 'important' : ''}</strong>
+      <strong>{` ${note.important ? 'important' : ''}`}</strong>
+      <button onClick={handleClick}> {note.important ? 'turn off' : 'turn on' }</button>
     </li>
   )
 }
