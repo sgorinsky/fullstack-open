e99c13b2b99ed7d87007da447cd18301ee84af72
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-17 21:16:14 -0600

    changed link text

--- 7/anecdotes-routed/src/App.js
+++ 7/anecdotes-routed/src/App.js
@@ -3,7 +3,7 @@ import Menu from './components/Menu'
 
 const Footer = () => (
   <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -sovelluskehitys</a>.
+    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Fullstack-open</a>.
 
     See <a href='https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
   </div>
