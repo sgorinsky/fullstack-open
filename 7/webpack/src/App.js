0a65b466dbb1e9d40dcfedc447a61835bf0ffbeb
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-22 23:10:14 -0500

    webpack dev server + react counter app example

--- 7/webpack/src/App.js
+++ 7/webpack/src/App.js
@@ -1,10 +1,17 @@
 import './index.css'
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => (
-  <div className='container'>
-    Hello, webpack
-  </div>
-)
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={() => setCounter(counter + 1)}>
+        press
+      </button>
+    </div>
+  )
+}
 
 export default App
