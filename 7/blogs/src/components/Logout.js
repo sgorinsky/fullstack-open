c776dfe79c80ed82d0bc91e6d8ab4cfe93c9d319
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-27 23:28:17 -0500

    inline handleClick for Logout

--- 7/blogs/src/components/Logout.js
+++ 7/blogs/src/components/Logout.js
@@ -4,21 +4,22 @@ import { connect } from 'react-redux'
 import { initializeBlogs } from '../reducers/blogs'
 
 const Logout = ({user, setUser, setError, setNotification, setBlogs, initializeBlogs}) => {
+    const handleClick = async () => {
+        const temp = user.username;
+        setUser(null)
+        window.localStorage.clear()
+        const blogs = initializeBlogs()
+        setBlogs(blogs)
+        setError(true);
+        setNotification(`${temp} has logged out`)
+        setTimeout(() => {
+            setError(false);
+            setNotification(null);
+        }, 1000)
+    }
+
     return (
-        <button onClick={async () => {
-            const temp = user.username;
-            setUser(null)
-            window.localStorage.clear()
-            const blogs = initializeBlogs()
-            setBlogs(blogs)
-            setError(true);
-            setNotification(`${temp} has logged out`)
-            setTimeout(() => {
-                setError(false);
-                setNotification(null);
-            }, 1000)
-            
-        }}> are you sure? </button>
+        <button onClick={handleClick}> are you sure? </button>
     )
 }
 
