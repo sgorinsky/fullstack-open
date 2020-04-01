eb95731c50209857cd2ad4dcd2157fd3c4a03658
Sam Gorinsky <sgorinsky@gmail.com>
2020-03-31 23:41:58 -0500

    bcrypt + jwt.verify login work

--- 4/blogs-backend/controllers/login.js
+++ 4/blogs-backend/controllers/login.js
@@ -3,40 +3,39 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter.post('/', async (request, response) => {
-    const body = request.body
-    const user = await User.findOne({ username: body.username })
-    
-    const hashedPass = await bcrypt.hash(body.password, 10)
+loginRouter.post('/', async (request, response, next) => {
+    try {
+        const body = request.body
+        const user = await User.findOne({ username: body.username })
 
-    const passwordCorrect = !user 
-    ? false 
-    // for some reason, hash at registration (POST request to /api/users) is different than hash used to compare
-    : await bcrypt.compare(body.password, hashedPass) || bcrypt.compare(body.password, user.passwordHash)
+        const passwordCorrect = !user
+            ? false
+            : await bcrypt.compare(body.password, user.passwordHash)
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({
-            error: 'invalid username or password'
-        })
+        if (!user || !passwordCorrect) {
+            return response.status(401).json({
+                error: 'invalid username or password'
+            })
+        }
+
+        // Something wrong with verify method of token
+        const token = user.token
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (typeof decodedToken !== 'object') {
+            return response.status(401).json({
+                error: decodedToken
+            })
+        }
+        const updatedUser = await User.findByIdAndUpdate(user.id, { token }, { upsert: true });
+
+        response
+            .status(200)
+            .send(updatedUser)
+
+    } catch(error) {
+        next(error)
     }
-
-    /*
-    const userForToken = {
-        username: user.username,
-        id: user._id,
-    }
-    */
-    
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvb3QiLCJpZCI6IjVkOTc4MjhmNzM5ZmUxYWNjYzJmYTllOCIsImlhdCI6MTU3MjE0ODQ1OX0.LrJk3mWXP2qQ-d1zaLwl_RqniapVu5lBfgVAJfENsRQ'
-
-    // Something wrong with verify method of token
-    // const decodedToken = jwt.verify(token, process.env.SECRET)
-
-    const updatedUser = await User.findByIdAndUpdate(user.id, { 'token': token }, { upsert: true });
-    
-    response
-        .status(200)
-        .send(updatedUser)
 })
 
 module.exports = loginRouter
