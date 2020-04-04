7caa8322c5807c2db84c47cdc314a25c29c7b2fb
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-03 20:51:19 -0500

    routing + navbar

--- 7/blogs/src/App.js
+++ 7/blogs/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect
+} from 'react-router-dom'
+import { Nav, Navbar } from 'react-bootstrap'
 
 // components
 import Notification from './components/Notification'
@@ -8,13 +13,27 @@ import Blogs from './pages/Blogs'
 import Users from './pages/Users'
 
 const App = () => {
-
+  const padding = { padding: 5 }
   return (
-    <>
+    <Router>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/users">users</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/blogs">blogs</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
       <Notification />
-      <Users />
-      <Blogs />
-    </>
+      <Route exact path='/users' render={() => <Users />} />
+      <Route exact path='/blogs' render={() => <Blogs />} />
+    </Router>
   )
 }
 
--- 7/blogs/src/index.js
+++ 7/blogs/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -7,9 +8,11 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, 
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>, 
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
