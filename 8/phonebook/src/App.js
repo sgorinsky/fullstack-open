89eed228303c12e5d8c0d1aec7f20e510ed17556
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-09 21:40:06 -0500

    onError key when bad data is through form

--- 8/phonebook/src/App.js
+++ 8/phonebook/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
