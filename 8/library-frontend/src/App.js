af93cba78409e8105a0ccbcb1a8f37e133f6f745
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-09 23:05:31 -0500

    view state on button click to toggle between Authors and Books

--- 8/library-frontend/src/App.js
+++ 8/library-frontend/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import { ALL_AUTHORS } from './queries';
 
 const App = () => {
   const result = useQuery(ALL_AUTHORS)
-
+  const [view, setView] = useState(false)
   if (result.loading) {
     return <div>loading...</div>
   }
 
   return (
     <div>
-      <Authors authors={result.data.allAuthors} />
+      <button onClick={() => setView(false)}>Authors</button>
+      <button onClick={() => setView(true)}>Books</button>
+      {!view ? <Authors authors={result.data.allAuthors} /> : null}
     </div>
   )
 }
