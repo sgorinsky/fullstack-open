7a68175e9b5059e54a7af5e98025aca9ea17093c
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-11 20:42:20 -0500

    mongoose/mongodb person model

---
+++ 8/graphql-phonebook-backend/models/Person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// Although there may be redundancy to require these fields since graphql verifies, 
+//    doesn't hurt to hold database to same standards
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5
+  },
+  phone: {
+    type: String,
+    minlength: 5
+  },
+  street: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+})
+
+module.exports = mongoose.model('Person', schema)
