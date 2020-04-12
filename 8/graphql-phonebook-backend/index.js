7f36658aee83e0e8f7c86b0fd56369aef2646f7b
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-11 22:09:25 -0500

    process.env.SECRET

--- 8/graphql-phonebook-backend/index.js
+++ 8/graphql-phonebook-backend/index.js
@@ -179,7 +179,7 @@ const server = new ApolloServer({
     const auth = req ? req.headers.authorization : null
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(
-        auth.substring(7), JWT_SECRET
+        auth.substring(7), process.env.SECRET
       )
       const currentUser = await User.findById(decodedToken.id).populate('friends')
       return { currentUser }
