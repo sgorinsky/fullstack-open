e48af5c4842286104ae300d0f393b6ec876463c3
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-19 01:40:06 -0500

    LoginForm with state and gql hooks

--- 8/phonebook-frontend/src/components/LoginForm.js
+++ 8/phonebook-frontend/src/components/LoginForm.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
 
 const LoginForm = ({ setToken, setError }) => {
-  const login = (event) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem('phonenumbers-user-token', token) // set browser cache
+    }
+  }, [result.data]) // eslint-disable-line
+
+  const submit = (event) => {
     event.preventDefault()
+    login({ variables: { username, password } })
   }
 
   return (
-    <form onSubmit={login}>
-      <input></input>
-      <button type='submit'>login</button>
-    </form>
+    <div>
+      <form onSubmit={submit}>
+        <input value={username} onChange={({ target }) => setUsername(target.value)} />
+        <input value={password} type='password' onChange={({ target }) => setPassword(target.value)} />
+        <button type='submit'>login</button>
+      </form>
+    </div>
   )
 }
 
