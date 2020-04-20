07f75b1a3e365c368e0588a51246c8c52f4070f5
Sam Gorinsky <sgorinsky@gmail.com>
2020-04-19 22:16:47 -0500

    boilerplate LoginForm

---
+++ 8/library-frontend/src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+
+const LoginForm = ({ setToken, setError }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem('phonenumbers-user-token', token) // set browser cache
+    }
+  }, [result.data]) // eslint-disable-line
+
+  const submit = (event) => {
+    event.preventDefault()
+    login({ variables: { username, password } })
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username
+          <input value={username} onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password
+          <input value={password} type='password' onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
